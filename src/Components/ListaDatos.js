094cbe6d03f655ac55525deb6759fe3f7e15adc0
BrandonArce27 <brandonarce2712@gmail.com>
2023-11-07 00:14:32 -0600

    Update ListaDatos.js

--- src/Components/ListaDatos.js
+++ src/Components/ListaDatos.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 export const ListaDatos = () => {
   const [usuarios, setUsuarios] = React.useState([]);
   const [albums, setAlbums] = React.useState([]);
@@ -40,16 +39,16 @@ export const ListaDatos = () => {
 
   return (
     //De esta manera renderizamos los datos y los relacionamos a la vez.
-    <div> 
+    <div>
       {usuarios.map((usuario) => (
         <div key={usuario.id}>
-          <h2>Usuario: {usuario.name}</h2>
+          <h2>Usuarios tareuki: {usuario.name}</h2>
           {albums
             .filter((album) => album.userId === usuario.id)
             .map((album) => (
               <div key={album.id}>
                 <h3>Album: {album.title}</h3>
-                <ul className="fotos" style={{display:"flex"}}>
+                <ul className="fotos" style={{ display: "flex" }}>
                   {fotos
                     .filter((foto) => foto.albumId === album.id)
                     .map((foto) => (
